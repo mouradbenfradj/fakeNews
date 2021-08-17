b2a3beb7bcb549185155692931b7d60f79b5542f
Mourad <mourad.ben.fradj@gmail.com>
2021-08-17 07:56:59 +0100

    authentificcation

---
+++ routes/auth.routes.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const {verifySignUp} = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+/* GET users listing. */
+//module.exports = function(router) {
+/*app.use(function(req, res, next) {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+});*/
+
+router.post(
+    "/signup",
+    [
+        verifySignUp.checkDuplicateUsernameOrEmail,
+        verifySignUp.checkRolesExisted
+    ],
+    controller.signup
+);
+
+router.post("/signin", controller.signin);
+module.exports = router;
+
+//};
